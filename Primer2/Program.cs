98062f6f9d0d24d596d1a2c50e7f3ae72cedb216
Aleksey Chernov <n9613522959@yandex.ru>
2022-06-27 11:59:42 +0300

    Задача 4 и Задача 6

---
+++ Primer2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+
+int a, b, c; 
+Console.WriteLine("Введите 1 число: ");
+a = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+b = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("Введите 3 число: ");
+c = Convert.ToInt32( Console.ReadLine());
+
+if (a > b && a > c)
+{
+ Console.WriteLine($"Max = {a}");
+}
+  else if (b > a && b > c )
+    {
+     Console.WriteLine($"Max = {b}");
+    }
+      else if (c > a && c > b)
+       {
+         Console.WriteLine($"Max = {c}");
+       }
+          else if (a == b && a == c)
+            {  
+             Console.WriteLine($"Числа равны");
+            }
